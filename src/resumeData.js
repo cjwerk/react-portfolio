8daf5fbe29aa68f650ee0da7c59f70632e330fd2
Chris Werkheiser <cjwerk@gmail.com>
2021-02-27 14:50:43 -0800

    resume data

---
+++ src/resumeData.js
@@ -0,0 +1,162 @@
+let resumeData = {
+  imagebaseurl: "https://cjwerk.github.io",
+  name: "Chris Werkheiser",
+  role: "full stack web developer",
+  linkedinId: "https://www.linkedin.com/in/chris-werkheiser-3375891bb/",
+  roleDescription:
+    "I love learning new technolgies, trained in both the front end development and backend I love to try new things and test out new tools",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/chris-werkheiser-3375891bb/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/cjwerk",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme:
+    "My name is Chris James Werkheiser, I was born in Las Vegas Nevada in the year 2000. When I was 4 my family moved to AZ and I have lived her ever since. When I was 9 years old I didn't know it then but I would meet the women who would become my wife, and on June 5th 2019 she did. I have always loved computer's and would often wonder how they would work, I only started learning how to code in 2020 but I am loving every minute of it. Few things intrigue me as much as learning to code. I am excited to continue to learn more and more about this exciting field, thanks to the wonderfull instructional staff at University of Arizona.",
+  address: "United States",
+  website: "",
+  education: [
+    {
+      UniversityName: "American High School",
+      specialization: "General education",
+      MonthOfPassing: "Feb",
+      YearOfPassing: "2019",
+      Achievments: "Honor role student",
+    },
+    {
+      UniversityName: "University Of Arizona Full Stack Coding Bootcamp",
+      specialization: "Full stack web development",
+      MonthOfPassing: "Mar",
+      YearOfPassing: "2021",
+      Achievments: "Employer competitive",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Thayne Stone Works",
+      specialization: "General labor",
+      MonthOfLeaving: "Aug",
+      YearOfLeaving: "2019",
+    },
+    {
+      CompanyName: "CVS Health",
+      specialization: "Benefits Verfication",
+      MonthOfLeaving: "Present",
+      YearOfLeaving: "Present",
+      Achievements: "Top preformer",
+    },
+  ],
+  skillsDescription: "Full stack development",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "ReactJs",
+    },
+    {
+      skillname: "JavaScript",
+    },
+    {
+      skillname: "Bootstrap",
+    },
+    {
+      skillname: "Bulma",
+    },
+    {
+      skillname: "Jquery",
+    },
+    {
+      skillname: "NodeJS",
+    },
+    {
+      skillname: "Jest",
+    },
+    {
+      skillname: "Express",
+    },
+    {
+      skillname: "MySQL",
+    },
+    {
+      skillname: "Sequelize",
+    },
+    {
+      skillname: "Handlebars",
+    },
+    {
+      skillname: "Prettier",
+    },
+    {
+      skillname: "Eslint",
+    },
+    {
+      skillname: "Great with people",
+    },
+    {
+      skillname: "Fast learner",
+    },
+    {
+      skillname: "Reliable",
+    },
+    {
+      skillname: "Out of the box thinking",
+    },
+    {
+      skillname: "Great with fast paced environment",
+    },
+  ],
+  portfolio: [
+    {
+      name: "",
+      description: "",
+      imgurl: "",
+    },
+    {
+      name: "",
+      description: "",
+      imgurl: "",
+    },
+    {
+      name: "",
+      description: "",
+      imgurl: "",
+    },
+    {
+      name: "",
+      description: "",
+      imgurl: "",
+    },
+    {
+      name: "",
+      description: "",
+      imgurl: "",
+    },
+    {
+      name: "",
+      description: "",
+      imgurl: "",
+    },
+  ],
+  testimonials:[
+      {
+          description: "",
+          name: ""
+      },
+      {
+        description: "",
+        name: ""
+    },
+  ]
+};
+
+export default resumeData
