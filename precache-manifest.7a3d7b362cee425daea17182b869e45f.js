7eb442dca9f9de4cc85ac997475b65b45976d58b
Chris Werkheiser <cjwerk@gmail.com>
2021-03-24 17:39:14 -0700

    Updates

---
+++ precache-manifest.7a3d7b362cee425daea17182b869e45f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6240806cdc577bb0d9741678fac462f",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "258b84c9e23493dc7d4e",
+    "url": "/react-portfolio/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "d63e89d434d5c9a8bd21",
+    "url": "/react-portfolio/static/js/2.b2f455bf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.b2f455bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "258b84c9e23493dc7d4e",
+    "url": "/react-portfolio/static/js/main.5555b5da.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  }
+]);
